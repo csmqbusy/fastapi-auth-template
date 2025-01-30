05a7d0469ace3022f560b6c2bf5503a432032f2c
csmqbusy <cosmiqeyes@yandex.by>
2025-01-30 17:35:56 +0300

    feat: add dependencies for getting payload of tokens

--- app/api/dependencies/authentication.py
+++ app/api/dependencies/authentication.py
@@ -1,7 +1,14 @@
-from fastapi import Form, Depends
+from fastapi import Form, Depends, HTTPException
+from jwt import InvalidTokenError
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+from starlette.requests import Request
 
-from app.auth.utils import verify_password
+from app.auth.utils import (
+    verify_password,
+    decode_refresh_token,
+    decode_access_token,
+)
 from app.db import get_db_session
 from app.services.user_service import get_user_by_username
 from app.api.exceptions.authentication import InvalidCredentialsException
@@ -18,3 +25,36 @@ async def validate_credentials(
     if not verify_password(password, user.password):
         raise InvalidCredentialsException()
     return user
+
+
+async def get_refresh_token_payload(request: Request) -> dict:
+    if not (refresh_token := request.cookies.get("refresh_token")):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token not found.",
+        )
+    try:
+        payload = decode_refresh_token(refresh_token)
+    except InvalidTokenError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token.",
+        )
+    return payload
+
+
+async def get_access_token_payload(request: Request) -> dict:
+    if not (access_token := request.cookies.get("access_token")):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token not found.",
+        )
+    try:
+        payload = decode_access_token(access_token)
+    except InvalidTokenError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token.",
+        )
+    return payload
+
