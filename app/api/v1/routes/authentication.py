ee408a64ed9bc5a1553e6a9c972cf3a35c46fabd
csmqbusy <cosmiqeyes@yandex.by>
2025-01-30 17:20:03 +0300

    feat: add login route

--- app/api/v1/routes/authentication.py
+++ app/api/v1/routes/authentication.py
@@ -3,23 +3,47 @@ from fastapi import (
     Depends,
     HTTPException,
     status,
+    Response,
 )
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.api.dependencies.authentication import validate_credentials
 from app.auth.utils import (
     hash_password,
+    create_access_token, create_refresh_token,
 )
 from app.db import get_db_session
 from app.exceptions.user import UsernameAlreadyExists, EmailAlreadyExists
-from app.schemas.user import SUserSignUp
+from app.schemas.user import SUserSignUp, SUserSignIn
 from app.services.user_service import create_user
 
 router = APIRouter()
 
 
 @router.post(
+    "/login",
+    summary="Authenticate a user",
+)
+async def auth_user_issue_jwt(
+    response: Response,
+    user: SUserSignIn = Depends(validate_credentials),
+):
+    payload = {"sub": user.username}
+    access_token = create_access_token(payload)
+    refresh_token = create_refresh_token(payload)
+    response.set_cookie(
+        key="access_token", value=access_token, httponly=True, secure=True,
+        samesite="lax"
+        )
+    response.set_cookie(
+        key="refresh_token", value=refresh_token, httponly=True
+        )
+    return {"sign_in": "Success!"}
+
+
+@router.post(
     "/registration/",
-    summary="Create new user.",
+    summary="Create new user",
     status_code=status.HTTP_201_CREATED,
 )
 async def sign_up_user(
