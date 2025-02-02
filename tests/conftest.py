1ed3d324a216ab4cf20df32419729dc534d5902f
csmqbusy <cosmiqeyes@yandex.by>
2025-02-02 11:20:55 +0300

    test: add base fixtures
    - test db creation
    - test client creation

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from starlette.testclient import TestClient
+
+from app.core.config import settings
+from app.db.dependencies import database_manager
+from app.main import main_app
+from app.models import Base
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def prepare_database():
+    assert settings.mode == "TEST"
+
+    async with database_manager.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+@pytest.fixture(scope="function")
+def client():
+    yield TestClient(main_app)
