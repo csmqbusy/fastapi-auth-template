6aa51b741b218243b59cf28ff8a008a01c5b4014
csmqbusy <cosmiqeyes@yandex.by>
2025-02-03 18:34:37 +0300

    test: add test for `_check_unique_username` function in user services

---
+++ tests/integration/auth/test_user_service.py
@@ -0,0 +1,36 @@
+import pytest
+from pydantic import EmailStr
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.repositories import user_repo
+from app.schemes.user import SUserSignUp
+from app.services.user import _check_unique_username
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "username, password, email",
+    [
+        (
+            "aubameyang",
+            "password",
+            "aubameyang@example.com",
+        ),
+    ]
+)
+async def test__check_unique_username(
+    db_session: AsyncSession,
+    username: str,
+    password: str,
+    email: EmailStr,
+):
+    assert await _check_unique_username(username, db_session) is True
+
+    user = SUserSignUp(
+        username=username,
+        password=password.encode(),
+        email=email,
+    )
+    await user_repo.add(db_session, user.model_dump())
+
+    assert await _check_unique_username(username, db_session) is False
