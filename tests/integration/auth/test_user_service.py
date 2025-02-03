c5ecc96e2a4f334d4a6960bc5bc0bcfbd3411180
csmqbusy <cosmiqeyes@yandex.by>
2025-02-03 18:35:33 +0300

    test: add test for `_check_unique_email` function in user services

--- tests/integration/auth/test_user_service.py
+++ tests/integration/auth/test_user_service.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.repositories import user_repo
 from app.schemes.user import SUserSignUp
-from app.services.user import _check_unique_username
+from app.services.user import _check_unique_username, _check_unique_email
 
 
 @pytest.mark.asyncio
@@ -34,3 +34,32 @@ async def test__check_unique_username(
     await user_repo.add(db_session, user.model_dump())
 
     assert await _check_unique_username(username, db_session) is False
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "username, password, email",
+    [
+        (
+            "ibrahimovic",
+            "password",
+            "ibrahimovic@example.com",
+        ),
+    ]
+)
+async def test__check_unique_email(
+    db_session: AsyncSession,
+    username: str,
+    password: str,
+    email: EmailStr,
+):
+    assert await _check_unique_email(email, db_session) is True
+
+    user = SUserSignUp(
+        username=username,
+        password=password.encode(),
+        email=email,
+    )
+    await user_repo.add(db_session, user.model_dump())
+
+    assert await _check_unique_email(email, db_session) is False
