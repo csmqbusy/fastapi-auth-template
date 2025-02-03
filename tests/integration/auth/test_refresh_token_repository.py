462d92fc016a33d712fcc522c3d57fb50320d22a
csmqbusy <cosmiqeyes@yandex.by>
2025-02-03 16:22:31 +0300

    test: add test for `get` method in `RefreshTokenRepository`

--- tests/integration/auth/test_refresh_token_repository.py
+++ tests/integration/auth/test_refresh_token_repository.py
@@ -10,7 +10,7 @@ from app.schemes.user import SUserSignUp
 async def _add_mock_users_to_db(db_session: AsyncSession, qty: int) -> None:
     for i in range(qty):
         user = SUserSignUp(
-            username=f"mock_user_{i}",
+            username=f"mock_user_{200 + i}",
             password="qwerty".encode(),
             email=f"mock_email_{i}@example.com",  # noqa
         )
@@ -68,4 +68,55 @@ async def test_add_refresh_token(
     assert added_token.expires_at == expires_at
     device_info_from_db_scheme = SDeviceInfo.model_validate(
         added_token.device_info)
+    assert device_info_from_db_scheme == device_info
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "user_id, token_hash, created_at, expires_at, device_info",
+    [
+        (
+            1,
+            "hash1",
+            1234567890,
+            1234567890 + 3600,
+            SDeviceInfo(user_agent="Mozilla/5.0", ip_address="192.168.1.1"),
+        ),
+        (
+            2,
+            "hash2",
+            1234567890,
+            1234567890 + 3600,
+            SDeviceInfo(user_agent="Mozilla/5.0", ip_address="192.168.1.2"),
+        ),
+    ]
+)
+async def test_get_refresh_token(
+    db_session: AsyncSession,
+    user_id: int,
+    token_hash: str,
+    created_at: int,
+    expires_at: int,
+    device_info: SDeviceInfo,
+):
+    all_users_in_db = await user_repo.get_all(db_session, {})
+    if len(all_users_in_db) < 10:
+        await _add_mock_users_to_db(db_session, 10)
+
+    refresh_token = SRefreshToken(
+        user_id=user_id,
+        token_hash=token_hash,
+        created_at=created_at,
+        expires_at=expires_at,
+        device_info=device_info,
+    )
+    token_from_db = await refresh_token_repo.add(
+        db_session, refresh_token.model_dump())
+    token_after_get = await refresh_token_repo.get(db_session, token_from_db.id)
+    assert token_after_get.id == token_from_db.id
+    assert token_after_get.user_id == user_id
+    assert token_after_get.token_hash == token_hash
+    device_info_from_db_scheme = SDeviceInfo.model_validate(
+        token_after_get.device_info
+    )
     assert device_info_from_db_scheme == device_info
