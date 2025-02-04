95bd4d72a5dd3a612571889021344f74cc23b0d0
csmqbusy <cosmiqeyes@yandex.by>
2025-02-04 16:06:02 +0300

    test: add test for `create_access_token` function in auth services

--- tests/unit/auth/test_auth_service.py
+++ tests/unit/auth/test_auth_service.py
@@ -1,4 +1,5 @@
 from contextlib import nullcontext
+from datetime import datetime, UTC
 from typing import ContextManager
 
 import pytest
@@ -8,6 +9,8 @@ from app.services.auth import (
     TokenType,
     get_token_iat_and_exp,
     SECS_IN_HOUR,
+    create_access_token,
+    decode_access_token,
 )
 
 
@@ -43,3 +46,25 @@ def test_get_token_iat_and_exp(
                 settings.auth.refresh_token_expires_days * SECS_IN_HOUR
             )
             assert exp - iat == refresh_token_expires_sec
+
+
+@pytest.mark.parametrize(
+    "payload",
+    [
+        {"sub": "etoo"},
+    ]
+)
+def test_create_access_token(
+    payload: dict,
+):
+    iat = int(datetime.now(UTC).timestamp())
+    exp = iat + 300
+    token = create_access_token(payload, iat, exp)
+    assert token is not None
+    assert isinstance(token, str)
+
+    decoded_payload = decode_access_token(token)
+    assert decoded_payload is not None
+    assert decoded_payload["sub"] == payload["sub"]
+    assert decoded_payload["iat"] == iat
+    assert decoded_payload["exp"] == exp
